a8a758c686e64ae5ca18b61ff0c4e57d5e6bf0e0
Tristan Barnes <tristan.barnes@colorado.edu>
2023-09-14 17:54:14 -0600

    Merging changes with return value 1

--- main.cpp
+++ main.cpp
@@ -9,9 +9,5 @@ std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
 
 int main(){
     //call each of the above functions
-<<<<<<< HEAD
-    return 2;
-=======
-    return 2;
->>>>>>> main
+    return 1;
 }
